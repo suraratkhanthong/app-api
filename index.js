af0262e2de808c0793bb26489497bc377387ccf7
suraratkhanthong <khanthongsurarat@gmail.com>
2023-01-17 23:15:15 +0700

    update text

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 const PORT = 4000;
 
 app.listen(PORT, ()=>{
-  console.log(`API llistenin${PORT}`);
+  console.log(`API listenin${PORT}`);
 })
 app.get("/", (req,res)=>{
   console.log("log running")
-   res.send("API running");
+   res.send("API running Index");
 })
 app.get("/product", (req,res)=>{
   console.log("log product")
-   res.send("API Product");
+   res.send("API Product Test");
 })
 module.exports = app;
