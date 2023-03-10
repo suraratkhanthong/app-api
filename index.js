b0b02c669d18f1bee7e5c4ef1555aa4c9538358c
suraratkhanthong <62281054+suraratkhanthong@users.noreply.github.com>
2023-03-10 11:54:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const path = require("path");
 
 app.use(express.static(path.join(__dirname,
-"public", "uploads")));
+"public")));
 
 const corsOptions = {
   //origin: 'http://localhost:3000',
